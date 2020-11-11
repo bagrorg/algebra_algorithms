5e0689614aad82221ebddc4d1c6530f9ae1fe3e7
bagrorg <kuldanvit@gmail.com>
2020-11-11 22:08:09 +0300

    - trash

--- computational_linear_algebra/obj/les_solve.d
+++
@@ -1,2 +0,0 @@
-obj/les_solve.o: src/les_solve.cpp include/les_solve.hpp \
- include/matrix.hpp include/matrix_impl.hpp
--- computational_linear_algebra/obj/main.d
+++
@@ -1,3 +0,0 @@
-obj/main.o: src/main.cpp include/matrix.hpp include/matrix_impl.hpp \
- include/matrix.hpp include/les_solve.hpp include/les_solve_impl.hpp \
- include/les_solve.hpp
